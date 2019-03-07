981968f82403236fb7beceba722987e57cd39c96
Joshua Frasier <jafrasier50@hotmail.com>
2019-03-06 18:15:47 -0600

    finished applications/ awaiting deployment

--- shared.js
+++ shared.js
@@ -4,18 +4,19 @@ var modalNoButton = document.querySelector(".modal__action--negative");
 var selectPlanButtons = document.querySelectorAll(".plan button");
 var toggleButton = document.querySelector(".toggle-button");
 var mobileNav = document.querySelector(".mobile-nav");
+var ctaButton = document.querySelector(".main-nav__item--cta");
 
 for (var i = 0; i < selectPlanButtons.length; i++) {
   selectPlanButtons[i].addEventListener("click", function() {
-    // modal.style.display = "block";
-    // backdrop.style.display = "block";
     modal.classList.add("open");
-    backdrop.classList.add("open");
+    backdrop.style.display = "block";
+    setTimeout(function() {
+      backdrop.classList.add("open");
+    }, 10);
   });
 }
 
 backdrop.addEventListener("click", function() {
-  //   mobileNav.style.display = "none";
   mobileNav.classList.remove("open");
   closeModal();
 });
@@ -24,18 +25,31 @@ if (modalNoButton) {
 }
 
 function closeModal() {
-  //   backdrop.style.display = "none";
-  //   modal.style.display = "none";
   if (modal) {
     modal.classList.remove("open");
   }
-
   backdrop.classList.remove("open");
+  setTimeout(function() {
+    backdrop.style.display = "none";
+  }, 200);
 }
 
 toggleButton.addEventListener("click", function() {
-  //   mobileNav.style.display = "block";
-  //   backdrop.style.display = "block";
   mobileNav.classList.add("open");
-  backdrop.classList.add("open");
+  backdrop.style.display = "block";
+  setTimeout(function() {
+    backdrop.classList.add("open");
+  }, 10);
 });
+
+// ctaButton.addEventListener("animationstart", function(event) {
+//   console.log("Animation Started!", event);
+// });
+
+// ctaButton.addEventListener("animationend", function(event) {
+//   console.log("Animation Ended", event);
+// });
+
+// ctaButton.addEventListener("animationiteration", function(event) {
+//   console.log("Animation iteration", event);
+// });
